8127b3753ae9aa0a9cfe07a2bfcb8b99e20d3bdf
Andrés Tercero <atercemo57@alumnes.ub.edu>
2025-09-25 13:30:35 +0200

    Segon save

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,12 @@
+
+
+
 public class Helloworld{
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+
+
+
+        System.out.println("Hello, World of Git!"); 
     }
 }
 
