c4e9636d8fa17e23043a16c85a7904b0acb7ad23
Andrés Tercero <atercemo57@alumnes.ub.edu>
2025-09-25 13:40:12 +0200

    Final save

--- Helloworld.java
+++ Helloworld.java
@@ -1,12 +1,6 @@
-
-
-
 public class Helloworld{
     public static void main(String[] args) {
-
-
-
-        System.out.println("Hello, World of Git!"); 
+        System.out.println("Hello, World");
     }
 }
 
